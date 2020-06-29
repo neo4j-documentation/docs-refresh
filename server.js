7a0d510abe8027a5f6cb487abaa509dbc7383928
Adam Cowley <github@adamcowley.co.uk>
2020-06-29 14:12:06 +0100

    Added dev server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static('./build/site'))
+
+app.get('/', (req, res) => res.redirect('/developer'))
+
+app.listen(8000, () => console.log('📘 http://localhost:8000'))
